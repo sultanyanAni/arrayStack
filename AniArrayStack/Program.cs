3258478316edd88d09255268b0ebd8022fa00812
Ani <sultanyanani@gmail.com>
2018-09-15 16:36:37 -0700

    fixed Push and Pop functions. Added IEnumerator function

--- AniArrayStack/Program.cs
+++ AniArrayStack/Program.cs
@@ -11,7 +11,22 @@ namespace AniArrayStack
         static void Main(string[] args)
         {
             Stack<int> stack = new Stack<int>();
-          
+            for (int i = 0; i < 11; i++)
+            {
+                stack.Push(i + 1);
+            }
+
+            Console.WriteLine(stack.Pop());
+
+
+            Console.ReadKey();
+            Console.Clear();
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
 
         }
     }
--- AniArrayStack/Stack.cs
+++ AniArrayStack/Stack.cs
@@ -9,48 +9,59 @@ namespace AniArrayStack
     class Stack<T>
     {
         T[] theStack;
-        int Count => theStack.Length;
+        int Capacity => theStack.Length;
+        int ItemCount = 0;
         public Stack()
         {
-            theStack = new T[0];
+            theStack = new T[5];
         }
-        public void Print()
+        public IEnumerator<T> GetEnumerator()
         {
-            for (int i = 0; i < theStack.Length; i++)
+            for (int i = ItemCount - 1; i >= 0; i--)
             {
-                Console.WriteLine(theStack[i]);
+                yield return theStack[i];
             }
         }
         public bool IsEmpty()
         {
             return theStack.Length == 0;
-          
+
         }
         public void Push(T itemToPush)
         {
-            T[] temp = new T[theStack.Length + 1];
-            temp[0] = itemToPush;
-            for (int i = 1; i < theStack.Length + 1; i++)
+            if (ItemCount >= Capacity)
             {
-                temp[i] = theStack[i - 1];
+                T[] temp = new T[theStack.Length * 2];
+
+                for (int i = 0; i < ItemCount; i++)
+                {
+                    temp[i] = theStack[i];
+                }
+                theStack = temp;
             }
-            theStack = temp;
+
+            theStack[ItemCount] = itemToPush;
+
+
+            ItemCount++;
         }
 
         public T Pop()
         {
-            if(IsEmpty())
+            if (IsEmpty())
             {
                 throw new Exception("The Stack Is Empty");
             }
-            T valueToCheck = theStack[0];
-            T[] temp = new T[theStack.Length - 1];
-            for (int i = 0; i < temp.Length; i++)
-            {
 
-            }
-            return valueToCheck;
 
+            //test this for more understanding
+            return theStack[ItemCount -= 1];
+        }
+
+        public T Peek()
+        {
+            T itemToPeek = theStack[ItemCount];
+            return itemToPeek;
         }
 
     }
